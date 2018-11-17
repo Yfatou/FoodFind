bf7507bd65b18cec1b6723f70532f4c3655b4de4
dissonance <ryanminor01u@yahoo.com>
2018-11-17 12:45:49 -0600

    added firebase initialize

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDECKdlUUwWo_jL0z_teQaPLBcSiwqRcIs",
+    authDomain: "food-project-4b9c6.firebaseapp.com",
+    databaseURL: "https://food-project-4b9c6.firebaseio.com",
+    projectId: "food-project-4b9c6",
+    storageBucket: "food-project-4b9c6.appspot.com",
+    messagingSenderId: "103528475709"
+  };
+  firebase.initializeApp(config);
